ac326b17d94b4b5fb324c68ceb8f05a4c7f338f6
Zach Eggert <egger0a6@email.arizona.edu>
2022-06-27 22:31:27 -0400

    add api interaction to fetch data from Dota2 API, add bootstrap cards to display all heroes on heroes/index page

--- controllers/heroes.js
+++ controllers/heroes.js
@@ -1,30 +1,20 @@
-import axios from "axios";
+import DotaWebApi from "dota-web-api";
+
+
+const api = new DotaWebApi(process.env.STEAM_WEB_API_KEY);
 
 function index(req, res) {
-  // const encodedParams = new URLSearchParams();
-  // encodedParams.append("apiKey", "41FB229C2B374BDE239E2F49954A702F");
-
-  // const options = {
-  //   method: 'POST',
-  //   url: 'https://dota2stefan-skliarovv1.p.rapidapi.com/getGameItems',
-  //   headers: {
-  //     'content-type': 'application/x-www-form-urlencoded',
-  //     'X-RapidAPI-Key': 'fb8c116b0dmsh1a944a77a0b0207p1ade21jsn953179516cf0',
-  //     'X-RapidAPI-Host': 'Dota2stefan-skliarovV1.p.rapidapi.com'
-  //   },
-  //   data: encodedParams
-  // };
-
-  // axios.request(options).then(function (response) {
-  //   console.log(response.data);
-  //   res.render("heroes/index", {
-  //     title: "All Heroes"
-  //   });
-  // }).catch(function (error) {
-  //   console.error(error);
-  // });
-  res.render("heroes/index", {
-    title: "All Heroes"
+  api.getHeroes()
+  .then((response) => {
+    console.log(response)
+    res.render("heroes/index", {
+      results: response,
+      title: "All Heroes"
+    })
+  })
+  .catch((err) => {
+    console.log(err);
+    res.redirect("/heroes");
   })
 }
 
